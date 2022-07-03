f6de616f9dab0a0d9372a3de2ca5a7f5a158711e
ftagliaca <filippotagliacarne@gmail.com>
2022-07-03 16:28:09 +0200

    added __init__

--- tools.py
+++ tools.py
@@ -8,7 +8,7 @@ def check_conseq_nums(arr: np.ndarray, min_conseq: int) -> bool:
     Check if there are consecutive numbers in the array
     """
 
-    for i, num in enumerate(arr):
+    for i, num in enumerate(arr[:-min_conseq]):
         if arr[i:i+min_conseq] == num:
             return True
     return False
