3e1f6ec38a6ae494b52f30ce6535c31dc5fbfab9
ftagliaca <filippotagliacarne@gmail.com>
2022-07-05 14:28:31 +0200

    modified tools

--- tests/tools_test.py
+++ tests/tools_test.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import unittest
+from itertools import product
 
 import numpy as np
 
@@ -14,6 +15,25 @@ class TestCheckConseqNums(unittest.TestCase):
         arr2 = np.array([1, 1, 1, 3, 4, 5])
         arr3 = np.array([1, 1, 1, 1, 1, 1])
 
-        self.assertFalse(tl.check_conseq_nums(arr1, 3))
-        self.assertTrue(tl.check_conseq_nums(arr2, 3))
-        self.assertTrue(tl.check_conseq_nums(arr3, 6))
+        arr1_ret = tl.check_conseq_nums(arr1, 3)
+        arr2_ret = tl.check_conseq_nums(arr2, 3)
+        arr3_ret = tl.check_conseq_nums(arr3, 6)
+        self.assertFalse(arr1_ret[0])
+        self.assertEqual(arr1_ret[1], -1)
+        self.assertTrue(arr2_ret[0])
+        self.assertEqual(arr2_ret[1], 1)
+        self.assertTrue(arr3_ret[0])
+        self.assertEqual(arr3_ret[1], 1)
+
+    def test_output_for_game(self):
+
+        for arr in map(np.array, product(range(3), repeat=7)):
+            center_number = arr[3]
+            win1 = np.all(arr[:4] == center_number)
+            win2 = np.all(arr[1:5] == center_number)
+            win3 = np.all(arr[2:6] == center_number)
+            win4 = np.all(arr[3:7] == center_number)
+            win = win1 or win2 or win3 or win4
+            ret = tl.check_conseq_nums(arr, 4)
+            self.assertEqual(ret[0], win)
+            self.assertEqual(ret[1], center_number if win else -1)
--- tools.py
+++ tools.py
@@ -3,15 +3,15 @@ from __future__ import annotations
 import numpy as np
 
 
-def check_conseq_nums(arr: np.ndarray, min_conseq: int) -> bool:
+def check_conseq_nums(arr: np.ndarray, min_conseq: int) -> tuple[bool, int]:
     """
     Check if there are consecutive numbers in the array
     """
 
     if arr.size < min_conseq:
-        return False
+        return False, -1
 
     for i, num in enumerate(arr[:-(min_conseq-1)]):
         if np.all(arr[i:i+min_conseq] == num):
-            return True
-    return False
+            return True, num
+    return False, -1
